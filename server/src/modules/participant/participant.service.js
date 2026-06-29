94b3e01ef3e84aa47d6d51a3971f581a0f86570c
praful-koli <prafulkoli852@gmail.com>
2026-06-30 02:52:38 +0530

    feat: Implement ParticipantService for managing room participants

---
+++ server/src/modules/participant/participant.service.js
@@ -0,0 +1,28 @@
+// participant.service.js
+import participantRepository from "./participant.repository.js";
+import roomRepository from "../room/room.repository.js";
+
+class ParticipantService {
+  async getRoomParticipants(roomCode) {
+    const room = await roomRepository.findByRoomCode(roomCode);
+    if (!room) throw new Error("Room not found");
+
+    return participantRepository.findByRoomId(room._id);
+  }
+
+  async updateSocket(participantId, socketId) {
+    return participantRepository.updateById(participantId, {
+      socketId,
+      isOnline: true,
+    });
+  }
+
+  async markOffline(participantId) {
+    return participantRepository.updateById(participantId, {
+      socketId: null,
+      isOnline: false,
+    });
+  }
+}
+
+export default new ParticipantService();
